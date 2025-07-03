d408105080d81bb9b90afb8e154024ef2190767f
姬飞雨 <453542772@qq.com>
2025-07-03 20:47:37 +0800

    Initial commit
    Project scaffolding and initial file setup.

---
+++ packages/frontend/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://tauri.app/start/frontend/nuxt/#update-nuxt-configuration
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+  devtools: { enabled: true },
+  modules: ['@nuxt/test-utils'],
+  ssr: false,
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  },
+})
