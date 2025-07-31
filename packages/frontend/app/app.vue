be245b128ce139f612e553f001814789e03d2278
姬飞雨 <453542772@qq.com>
2025-07-31 14:48:34 +0800

    Add Nuxt 4 support

---
+++ packages/frontend/app/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template lang="pug">
+NuxtLayout
+  NuxtPage
+</template>
+
+<style scoped lang="sass">
+</style>
---
+++ packages/frontend/app/layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template lang="pug">
+slot
+</template>
+
+<style scoped lang="sass">
+</style>
